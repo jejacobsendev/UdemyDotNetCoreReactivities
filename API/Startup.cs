d89925c905ebb0eb38c4b7e33480c8919401e187
Joel Jacobsen <jejacobsendev@gmail.com>
2022-08-08 22:20:20 -0400

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -24,6 +24,13 @@ public class Startup
         {
             opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
         });
+        services.AddCors(opt =>
+        {
+            opt.AddPolicy("CorsPolicy", policy =>
+            {
+                policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+            });
+        });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +46,9 @@ public class Startup
         // app.UseHttpsRedirection();
 
         app.UseRouting();
+
+        // Put UseCors directly after UseRouting
+        app.UseCors("CorsPolicy");
 
         app.UseAuthorization();
 
